1cbc2eba28cb54b9ae447c4157c19a7e9e877967
Suyog Kulkarni <suyogsk.143@gmail.com>
2022-04-27 23:12:37 +0530

    Assignment 1

---
+++ VariableExample.java
@@ -0,0 +1,18 @@
+class VariableExample {
+
+    public static void main(String args[])
+
+    {
+
+        int x = 10;
+        float y = 20.54f;
+        char z = 'x';
+        String message = "I am the best";
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(message);
+
+    }
+
+}
