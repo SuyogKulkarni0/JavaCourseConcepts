6c14b07fdfc40b74df88486345aa76e90449fe6e
Suyog Kulkarni <suyogsk.143@gmail.com>
2022-04-30 12:12:07 +0530

    Added Files for the Command line Arguments and Operators

---
+++ Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 4, b, c;
+        b = ++a;
+        System.out.println(b);
+        c = a++;
+        System.out.println(c);
+        System.out.println(a);
+    }
+
+}
